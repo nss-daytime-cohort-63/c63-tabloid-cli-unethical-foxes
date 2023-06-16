2b9ebd8a043e8cea816329c51b45ae3e35fc7329
sergey-mik <intelsurge@gmail.com>
2023-06-16 09:15:58 -0500

    note_management_note_list

--- TabloidCLI/UserInterfaceManagers/NoteManager.cs
+++ TabloidCLI/UserInterfaceManagers/NoteManager.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    internal class NoteManager
+    public class NoteManager : IUserInterfaceManager
     {
+        private readonly IUserInterfaceManager _parentUI;
+        private NoteRepository _noteRepository;
+        private PostRepository _postRepository;
+        private string _connectionString;
+        private readonly int _postId;
+
+        public NoteManager(IUserInterfaceManager parentUI, string connectionString, int postId)
+        {
+            _parentUI = parentUI;
+            _noteRepository = new NoteRepository(connectionString);
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+            _postId = postId;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Note Management Menu");
+            Console.WriteLine(" 1) List Notes");
+            Console.WriteLine(" 2) Add Note");
+            Console.WriteLine(" 3) Remove Note");
+            Console.WriteLine(" 0) Return");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Notes List");
+                    List();
+                    Console.WriteLine();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "3":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Note> notes = _noteRepository.GetAll();
+            foreach (Note note in notes)
+            {
+                Console.WriteLine(note);
+            }
+        }
+        private void Add()
+        {
+            Console.WriteLine("Add Note");
+            Note note = new Note();
+
+            Console.Write("Title: ");
+            note.Title = Console.ReadLine();
+
+            Console.Write("Text content: ");
+            note.Content = Console.ReadLine();
+
+            _noteRepository.Insert(note);
+        }
+
+        private void Remove()
+        {
+           //
+        }
+
+        
     }
 }
